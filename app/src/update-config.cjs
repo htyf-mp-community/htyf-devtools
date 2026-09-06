6569e9c36405e691dfc29e0a63ec0ff4057e576f
dgz <dagouzhi@qq.com>
2026-09-06 23:47:26 +0800

    fix

--- app/src/update-config.cjs
+++ app/src/update-config.cjs
@@ -1,4 +1,12 @@
-const DEFAULT_UPDATE_URL = 'https://dagouzhi.oss-cn-qingdao.aliyuncs.com/com.dagouzhi.mp.devtools/latest/';
+const os = require('os');
+let platform = '';
+if (platform === 'darwin') {
+  platform = 'mac';
+} else if (platform === 'win32') {
+  platform = 'win';
+}
+
+const DEFAULT_UPDATE_URL = `https://dagouzhi.oss-cn-qingdao.aliyuncs.com/com.dagouzhi.mp.devtools/latest/${platform}`;
 
 function getUpdateUrl(environment = process.env) {
   return (environment.HTYF_DEVTOOLS_UPDATE_URL || DEFAULT_UPDATE_URL).replace(/\/$/, '');
